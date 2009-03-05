456257002813ac43b813f4a5fb10bdb9f8d2fe1e
Eric Naeseth <enaeseth@gmail.com>
2009-03-04 19:27:54 -0600

    Adding the bare bones of the package.

---
+++ lastfm/__init__.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+"""
+A library for using the last.fm API from Python.
+"""
+
+__author__ = "Eric Naeseth <enaeseth@gmail.com>"
+__version__ = "0.1.0"
+__copyright__ = "Copyright © 2009 Eric Naeseth"
+__license__   = "MIT"
+
+__all__ = ['api', 'caching']
