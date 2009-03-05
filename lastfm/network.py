312dc5e9b3be674acdefb7557b94f9ae9dfe7395
Eric Naeseth <enaeseth@gmail.com>
2009-03-04 20:40:11 -0600

    Updating Agent to more easily handle GET and POST requests and add parameters from dictionaries.

--- lastfm/network.py
+++ lastfm/network.py
@@ -4,10 +4,15 @@
 Handles the low-level details of communicating with last.fm over HTTP.
 """
 
+from urllib import urlencode
 import urllib2
+from urlparse import urlparse, parse_qs, urlunparse
 import sys
 
 __version__ = '0.1'
+
+def unparse_qs(params):
+    return '&'.join('%s=%s' % (k, urlencode(v)) for k, v in params.iteritems())
 
 class Agent(object):
     """
@@ -23,11 +28,33 @@ class Agent(object):
             ('User-Agent', self._user_agent)
         ]
         
-    def open(url, data=None):
+    def get(self, url, data=None):
         """
-        Opens an HTTP connection and sends a request.
+        Opens an HTTP connection and sends a GET request.
+        The parameters in `params` are added as GET parameters.
         """
+        return self._opener.open(self._add_params(url, data or {}))
+        
+    def post(self, url, data=None):
+        """
+        Opens an HTTP connection and sends a POST request.
+        If `data` is a dictionary, it is first converted to URL-encoded
+        parameters.
+        """
+        if isinstance(data, dict):
+            data = unparse_qs(data)
+        
         return self._opener.open(url, data)
+        
+    def _add_params(self, url, params):
+        """Appends GET parameters to the URL and returns the result."""
+        
+        parsed = urlparse(url)
+        url_params = parse_qs(parsed.query)
+        url_params.update(params)
+        parsed.query = unparse_qs(url_params)
+        
+        return urlunparse(parsed)
 
     @property
     def _user_agent(self):
