9b6f410f686b58dc29cc49d7a3d6d55b10736dfb
Eric Naeseth <enaeseth@gmail.com>
2009-03-04 23:52:20 -0600

    Adding timestamp parsing.

--- lastfm/data.py
+++ lastfm/data.py
@@ -4,6 +4,9 @@
 Common data types used elsewhere in the library.
 """
 
+from email.utils import parsedate
+from datetime import datetime
+
 class Image(object):
     """An image served by last.fm."""
     
@@ -28,3 +31,10 @@ class Image(object):
         if not isinstance(other, Image):
             return False
         return other.url == self.url and other.size == self.size
+
+def parse_timestamp(stamp):
+    """
+    Parses an RFC822 timestamp as used by last.fm and returns a
+    datetime.datetime object representing that time.
+    """
+    return datetime(*parsedate(stamp)[:6])
